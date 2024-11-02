49d5701127b2531ddfd75a174751bf283336039c
Shiv Shankar <ps431352@gmail.com>
2024-11-02 20:45:05 +0530

    Add image for link

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,8 @@ import localFont from "next/font/local"
 import { ThemeProvider } from "@/components/theme-provider"
 import Navbar from "@/components/Navbar"
 import "./globals.css"
+import Head from "next/head"
+import img from "../public/link.png"
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -17,11 +19,28 @@ const geistMono = localFont({
 export const metadata = {
   title: "Quronymous - Anonymous Messaging for Quora Users",
   description: "Send and receive anonymous messages in a Quora-inspired platform",
+  image: `${img}`,
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        
+        {/* Open Graph Meta Tags */}
+        <meta property="og:title" content="Quronymous - Anonymous Messaging for Quora Users" />
+        <meta property="og:description" content="Send and receive anonymous messages in a Quora-inspired platform" />
+        <meta property="og:image" content={img} />
+        <meta property="og:url" content="https://quronymous.165131.xyz/" />
+        
+        {/* Twitter Meta Tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Quronymous - Anonymous Messaging for Quora Users" />
+        <meta name="twitter:description" content="Send and receive anonymous messages in a Quora-inspired platform" />
+        <meta name="twitter:image" content={img} />
+      </Head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-50 dark:bg-gray-900`}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <div className="flex flex-col min-h-screen">
